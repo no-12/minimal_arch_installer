102e6406f1fbc07d00c7cc64926c209fdff51b36
Nico Ohnezat <nico@no-12.net>
2020-12-05 22:24:51 +0100

    remove existing test coverage output before running a new one

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -eu
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+rm -rf "${SCRIPT_DIR}/test_coverage"
 kcov --include-path="${SCRIPT_DIR}/mai.sh" "${SCRIPT_DIR}/test_coverage" "${SCRIPT_DIR}/run_tests.sh"
